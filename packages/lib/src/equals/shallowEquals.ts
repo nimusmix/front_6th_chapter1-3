9c48d835879ac997489037e62bc8b43642daac27
nimusmix <nimusmix@gmail.com>
2025-07-21 01:14:01 +0900

    feat: shallowEquals 함수 구현

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,19 @@
+import { isObject } from "../utils/is";
+
 export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  if (a === b) return true;
+
+  if (!isObject(a) || !isObject(b)) return false;
+
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+
+  if (aKeys.length !== bKeys.length) return false;
+
+  for (const key of aKeys) {
+    if (!Object.prototype.hasOwnProperty.call(b, key)) return false;
+    if (a[key] !== b[key]) return false;
+  }
+
+  return true;
 };
