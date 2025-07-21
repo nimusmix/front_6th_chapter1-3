38c526b0f9189ba78f1e5d2cf4ad1d371db11ded
nimusmix <nimusmix@gmail.com>
2025-07-21 23:29:37 +0900

    feat: useCallback & useDeepMemo 구현

--- packages/lib/src/hooks/useDeepMemo.ts
+++ packages/lib/src/hooks/useDeepMemo.ts
@@ -4,6 +4,5 @@ import { useMemo } from "./useMemo";
 import { deepEquals } from "../equals";
 
 export function useDeepMemo<T>(factory: () => T, deps: DependencyList): T {
-  // 직접 작성한 useMemo를 참고해서 만들어보세요.
   return useMemo(factory, deps, deepEquals);
 }
